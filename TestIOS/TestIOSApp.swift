6f447aa92c7d9e2b93afbcc6fa7cfda540704c8e
Santhosh Banda <banda.santhosh007@gmail.com>
2023-11-03 17:02:15 +0530

    Initial Commit

---
+++ TestIOS/TestIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestIOSApp.swift
+//  TestIOS
+//
+//  Created by Santhosh Banda on 03/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TestIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
